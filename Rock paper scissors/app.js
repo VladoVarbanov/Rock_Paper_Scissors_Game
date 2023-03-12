2b79dbe925578200290d28b79981b75635dc76c1
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-03-12 16:14:37 +0200

    new tasks

---
+++ Rock paper scissors/app.js
@@ -0,0 +1,28 @@
+const computerChoiceDisplay = document.body.getElementById("computer-choice");
+const userChoiceDisplay = document.body.getElementById("user-choice");
+const resultDisplay = document.body.getElementById("result");
+const possibleChoices = document.body.querySelectorAll("button");
+let userChoice;
+let computerChoice;
+possibleChoices.forEach(possibleChoice =>
+  possibleChoice.addEventListener("click", e => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+  })
+);
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "scissors";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "paper";
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
